3d556207a907f37e7a885d59da27793f2061a8ce
Milad Nekofar <milad@nekofar.com>
2022-10-21 15:24:32 +0330

    fix: solve a type issues related to `wagmi` new version

--- components/ens-name.tsx
+++ components/ens-name.tsx
@@ -1,8 +1,8 @@
-import {useEnsName} from "wagmi";
+import {Address, useEnsName} from "wagmi";
 
 export const EnsName = (props: { address: string }) => {
   const {data: ensName} = useEnsName({
-    address: props.address,
+    address: props.address as Address,
   })
 
   let addressOrEnsName = ensName ?? `${props.address.substring(0, 6)}...${props.address.substring(38, 42)}` as const;
