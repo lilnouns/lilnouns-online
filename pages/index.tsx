d47da0c003ebcd1e9d489fff5e0c59abfcace110
Milad Nekofar <milad@nekofar.com>
2023-01-18 11:55:37 +0330

    refactor: add missing `tw-` prefix for css classes

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import type {NextPage} from "next";
 import Head from "next/head";
 import Header from "../components/header";
 import Content from "../components/content";
-import Footer from "../components/footer";
 
 const Home: NextPage = () => {
   return (
@@ -13,11 +12,11 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico"/>
       </Head>
 
-      <div className="relative bg-gray-50 overflow-hidden">
-        <div className="hidden sm:block sm:absolute sm:inset-y-0 sm:h-full sm:w-full" aria-hidden="true">
-          <div className="relative h-full max-w-7xl mx-auto">
+      <div className="tw-relative tw-bg-gray-50 tw-overflow-hidden">
+        <div className="tw-hidden sm:tw-block sm:tw-absolute sm:tw-inset-y-0 sm:tw-h-full sm:tw-w-full" aria-hidden="true">
+          <div className="tw-relative tw-h-full tw-max-w-7xl tw-mx-auto">
             <svg
-              className="absolute right-full transform translate-y-1/4 translate-x-1/4 lg:translate-x-1/2"
+              className="tw-absolute tw-right-full tw-transform tw-translate-y-1/4 tw-translate-x-1/4 lg:tw-translate-x-1/2"
               width={404}
               height={784}
               fill="none"
@@ -32,13 +31,13 @@ const Home: NextPage = () => {
                   height={20}
                   patternUnits="userSpaceOnUse"
                 >
-                  <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  <rect x={0} y={0} width={4} height={4} className="tw-text-gray-200" fill="currentColor" />
                 </pattern>
               </defs>
               <rect width={404} height={784} fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)" />
             </svg>
             <svg
-              className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 md:-translate-y-1/2 lg:-translate-x-1/2"
+              className="tw-absolute tw-left-full tw-transform -tw-translate-y-3/4 -tw-translate-x-1/4 md:-tw-translate-y-1/2 lg:-tw-translate-x-1/2"
               width={404}
               height={784}
               fill="none"
@@ -53,7 +52,7 @@ const Home: NextPage = () => {
                   height={20}
                   patternUnits="userSpaceOnUse"
                 >
-                  <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  <rect x={0} y={0} width={4} height={4} className="tw-text-gray-200" fill="currentColor" />
                 </pattern>
               </defs>
               <rect width={404} height={784} fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
@@ -61,7 +60,7 @@ const Home: NextPage = () => {
           </div>
         </div>
 
-        <div className="relative pt-6">
+        <div className="tw-relative tw-pt-6">
           <Header />
 
           <Content />
